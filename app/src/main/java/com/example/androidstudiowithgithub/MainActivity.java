cd70bf867ef740a8f049a42fcc8143de3c39fdda
Thiha Eung <thihaaungpol18@gmail.com>
2020-01-21 11:10:26 +0630

    version 0.1

--- app/src/main/java/com/example/androidstudiowithgithub/MainActivity.java
+++ app/src/main/java/com/example/androidstudiowithgithub/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.androidstudiowithgithub;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //version 0.1
+        Log.i(TAG, "onCreate: Started");
+
     }
 }
