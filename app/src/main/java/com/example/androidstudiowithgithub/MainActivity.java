471850d8018ac874ae3963092d0151e380513967
Thiha Eung <thihaaungpol18@gmail.com>
2020-01-21 11:04:09 +0630

    Version 0.0

---
+++ app/src/main/java/com/example/androidstudiowithgithub/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.androidstudiowithgithub;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
